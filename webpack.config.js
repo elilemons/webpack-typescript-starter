5867ffd718a947fdace719773dc528505401c683
Lisa Lemons <llemons@kpmg.com>
2019-04-17 14:56:12 -0400

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: "./src/index.ts"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Lisa Lemons' Webpack/Typescript Starter Kit"
+    })
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+};
